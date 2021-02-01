c9978c3cf77ddf84af9e8d450cae68c26b193b5c
ingridgrenholm <65853333+ingridgrenholm@users.noreply.github.com>
2021-02-01 15:05:26 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <template>
   <div id="app">
 
+    <sidebar />
+    <categories />
+
   </div>
 </template>
 
 <script>
 
 import Sidebar from './components/sidebar.vue'
+import Categories from './components/categories.vue'
 
 export default {
   name: 'App',
