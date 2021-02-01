ab2048abe641048130a196c6516d6803c8bf86f8
ingridgrenholm <65853333+ingridgrenholm@users.noreply.github.com>
2021-02-01 15:16:24 +0100

    test
    test

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <sidebar />
+    <categories />
   </div>
 </template>
 
 <script>
 import Sidebar from './components/sidebar.vue'
+import Categories from './components/categories.vue'
 
 export default {
   name: 'App',
   components: {
     Sidebar,
+    Categories,
   }
 }
 </script>
