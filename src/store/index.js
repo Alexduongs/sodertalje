c3b059172a8609aa02629c5dcb17751e7e1d6e9e
electricGebring <56607795+electricGebring@users.noreply.github.com>
2021-03-03 10:43:58 +0100

    Revert "vue 3"

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,16 @@
+import Vue from 'vue'
 import Vuex from 'vuex'
+
+Vue.use(Vuex)
 
 import * as actions from './actions'
 import * as mutations from './mutations'
 import * as getters from './getters'
 import state from './state'
 
-export default Vuex.createStore({
+export default new Vuex.Store({
   state,
   getters,
   mutations,
   actions,
-});
+})
