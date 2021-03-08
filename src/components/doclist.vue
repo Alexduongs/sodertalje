d0666a91cb0eaea17dc241748532370dde52afd0
ingridgrenholm <65853333+ingridgrenholm@users.noreply.github.com>
2021-03-08 13:18:48 +0100

    Visa Alla knapp, rensat kod
    Visa Alla knapp, rensat kod

--- src/components/doclist.vue
+++ src/components/doclist.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="section">
     <div class="hej" v-for="item in doclist" :key="item.Label" :item="item">
-      <div v-if="item.Thematic === $route.params.Thematic">
+      <div v-if= "item.Thematic === $route.params.Thematic ? $route.params.Thematic : $route.params.Elements">
         <div class="section">
           <div class="img-container">
             <img v-bind:src="item.CoverImg" width="100" height="130" />
@@ -27,34 +27,7 @@
           </div>
         </div>
       </div>
-
-      <div v-else-if="$route.params.Elements">
-        <div class="section">
-          <div class="img-container">
-            <img v-bind:src="item.CoverImg" width="100" height="130" />
-          </div>
-          <div
-            class="title"
-            :style="[
-              item.Status == 'Gällande'
-                ? { background: '#87BDC9' }
-                : item.Status == 'Remiss'
-                ? { background: '#E9A15F' }
-                : item.Status == 'Under framtagande'
-                ? { background: '#5F8C9A' }
-                : { background: '#C45941' },
-            ]"
-          >
-            <span class="labelTitle">
-              {{ item.Label }}
-            </span>
-            <div class="type">
-              <span>{{ item.Documenttype }} </span>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+ </div>
   </div>
 </template>
 
